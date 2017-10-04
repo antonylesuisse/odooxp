873f57a9a16b88aa55bf5e5c118896548f76de53
Denis Vermylen <dve@odoo.com>
2017-10-04 08:41:46 +0200

    [ADD] tyrell_corp: add friendly partners

--- tyrell_corp/__manifest__.py
+++ tyrell_corp/__manifest__.py
@@ -28,6 +28,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'data/tyrell_corp_data.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
